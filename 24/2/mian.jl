872e0f923606c9df3e12eef2503beee1e6558ee8
Mats Hoem Olsen <mats.hoem.olsen@nmbu.no>
2024-12-08 00:32:38 +0100

    refactored day 2 + solved day 2 part 2

--- 24/2/mian.jl
+++ 24/2/mian.jl
@@ -1,29 +1,38 @@
 
+function ValidateSequence(seq)
+    diff_test = diff(seq)
+    sign_test = [sign(x) for x in diff_test]
+    if any(x -> x != sign_test[1],sign_test)
+        return false
+    end
+    if any(x -> abs(x) > 3 || abs(x) < 1,diff_test)
+        return false
+    end
+
+    return true
+end
+
+function RemoveInvalid(seq)
+    for i in 1:length(seq)
+        if ValidateSequence(vcat(seq[1:i-1],seq[i+1:end]))
+            return true
+        end
+    end
+    return false
+end
 
 open(".\\2\\input","r") do f
     valids = 0
+    dampend_valids = 0
     while ! eof(f)
         s = [parse(Int, ss) for ss in split(readline(f))]
-        previus_number = s[1]
-        sign_of_values = 0
-        found_valid = true
-        for values in s[2:length(s)]
-            diff = values - previus_number
-            if abs(diff) < 1 || abs(diff) > 3
-                found_valid = false
-                break
-            end
-            if sign_of_values == 0
-                sign_of_values = sign(diff)
-            elseif sign(diff) != sign_of_values 
-                found_valid = false
-                break
-            end
-            previus_number = values
-        end
-        if found_valid
+        if ValidateSequence(s)
             valids += 1
+        elseif RemoveInvalid(s)
+            dampend_valids += 1
         end
     end
-    println(valids)
+    println("Part 1:" * string(valids))
+    println("Part 2:" * string(dampend_valids + valids))
+
 end
